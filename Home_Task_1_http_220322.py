6ceae9bcb6973eda1de7f69e72a827c19d5a1e0a
Олег Матшин <89175955650@mail.ru>
2022-03-22 23:27:45 +0500

    Add most_intell_hero method

--- Home_Task_1_http_220322.py
+++ Home_Task_1_http_220322.py
@@ -12,11 +12,26 @@ def input_heroes():
             break
         else:
             list_heroes.append(name)
-    return (list_heroes)
+    return list_heroes
 
-def most_intelligence():
-    url = "https://www.superheroapi.com/api.php/2619421814940190/search/Captain_America"
-    resp = requests.get(url)
-    pprint(resp.json())
+# creating the dictionary of heroes with their intelligences
+def dict_heroes_intel():
+    heroes = input_heroes()
+    print(f'You are input the names: {heroes}.')
+    heroes_intell_dict = {}
+    for item in heroes:
+        url = f"https://www.superheroapi.com/api.php/2619421814940190/search/{item}"
+        resp_name = requests.get(url)
+        int_hero = resp_name.json()['results'][0]['powerstats']['intelligence']
+        heroes_intell_dict[item] = int(int_hero)
+    return heroes_intell_dict
 
-input_heroes()
+# the method of choosing most intelligence hero 
+def most_intell_hero():
+    intel_hero = dict_heroes_intel()
+    intel_hero_sorted = sorted(intel_hero.items(), key=lambda item: item[1])
+    best_hero = intel_hero_sorted[(len(intel_hero_sorted) - 1)][0]
+    intell_level = intel_hero_sorted[(len(intel_hero_sorted) - 1)][1]
+    print(f'The heroes are: {intel_hero_sorted}.\nThe most intelligence hero is {best_hero} with the grade intelligence {intell_level}.')
+
+most_intell_hero()
