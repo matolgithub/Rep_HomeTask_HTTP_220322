5bd13e0d9d65142a52fcf53cb577719d5949c116
Олег Матшин <89175955650@mail.ru>
2022-03-23 22:58:14 +0500

    Maked upload method

--- Home_Task_2_http_230322.py
+++ Home_Task_2_http_230322.py
@@ -1,30 +1,32 @@
 import requests
 
+import os
+
 from pprint import pprint
+
 
 class YaUploader:
     def __init__(self, token: str):
         self.token = token
 
+    def get_headers(self):  
+        return {'Content-Type' : 'application/json', 'Authorization' : f'OAuth {self.token}'}
+
     def upload(self, file_path: str):
-        """Метод загружает файлы по списку file_list на яндекс диск"""
-        # Тут ваша логика
-        # Функция может ничего не возвращать
+        """Метод загруджает файл file_path на Яндекс-Диск"""
 
-    def get_headers(self):
-        return {'Content-Type' : 'application/json', 'Authorization' : 'OAuth {}'.format(self, token)}
-
-    def get_upload_link(self, disk_file_path):
+        file_path = os.path.normpath(file_path)
+        data = {"file": open(file_path, 'rb')}
         upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
         headers = self.get_headers()
-
+        params = {'path' : file_path, 'overwrite' : 'true'}
+        response = requests.get(upload_url, params=params, headers=headers)
+        href_json = response.json().get('href')
+        response_upload = requests.put(url=href_json, files=data, headers={})
+        pprint(response.json())
+        print(f'The result of PUT-operation is: "{response_upload.status_code}". Successfuly!')
 
 if __name__ == '__main__':
     # Получить путь к загружаемому файлу и токен от пользователя
-    path_to_file = ...
-    token = "AQAAAAANuuWFAADLW1koNP7ciknerimibi-F350"
-    uploader = YaUploader(token)
-    result = uploader.upload(path_to_file)
-
-    # token = "AQAAAAANuuWFAADLW1koNP7ciknerimibi-F350"
-   
+    token = "..."
+    result = YaUploader(token).upload('1.txt')
